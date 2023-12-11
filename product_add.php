3688e255f76ba86f44310bdbeb4c3adfba22fbc3
Yinon Bar <yinonbar1988@gmail.com>
2023-12-11 13:50:10 +0200

    products list

--- product_add.php
+++ product_add.php
@@ -13,11 +13,28 @@ if (isset($_POST['add_product'])) {
   $product_sell = $_POST['txtPrice'];
   $product_stock = $_POST['txtStock'];
   $product_description = $_POST['txtDesc'];
-  // $product_img = $_POST['txtImage'];
   $f_name = $_FILES['txtImage']['name'];
   $f_temp = $_FILES['txtImage']['tmp_name'];
-  // $f_size = $_FILES['txtImage']['size'];
+  $f_location = "upload/" . $f_name;
   move_uploaded_file($f_temp, "upload/" . $f_name);
+  $query = $pdo->prepare(
+    "INSERT INTO 
+    tbl_product (p_name, p_category, purchase_price, sell_price, p_stock, p_desc, p_image) 
+    VALUES 
+    (:product_name, :product_category, :product_purchase, :product_sell, :product_stock, :product_description, :f_location)"
+  );
+  $query->bindParam(':product_name', $product_name);
+  $query->bindParam(':product_category', $product_category);
+  $query->bindParam(':product_purchase', $product_purchase);
+  $query->bindParam(':product_sell', $product_sell);
+  $query->bindParam(':product_stock', $product_stock);
+  $query->bindParam(':product_description', $product_description);
+  $query->bindParam(':f_location', $f_location);
+  if ($query->execute()) {
+    $messages[] = "Product Added!";
+  } else {
+    echo "Error";
+  }
 }
 ?>
 <!-- Content Wrapper. Contains page content -->
@@ -32,32 +49,38 @@ if (isset($_POST['add_product'])) {
       <li class="active">Here</li>
     </ol>
   </section>
-
   <!-- Main content -->
   <section class="content container-fluid">
-
     <div class="box box-info">
+      <?php
+      if (isset($errors)) {
+        foreach ($errors as $error) { ?>
+          <div style="font-weight: bold; padding:1rem;" class="bg-danger text-danger">
+            <?= $error; ?>
+          </div>
+      <?php }
+      } ?>
+      <?php
+      if (isset($messages)) {
+        foreach ($messages as $message) { ?>
+          <div style="font-weight: bold; padding:1rem;" class="bg-success text-success">
+            <?= $message; ?>
+          </div>
+      <?php }
+      } ?>
       <div class="box-header with-border">
         <h3 class="box-title">Product</h3>
       </div>
       <form role="form" method="post" enctype="multipart/form-data">
         <div class="box-body">
           <div class="col-md-6">
-            <div class="bg-danger text-danger padding-3">
-              <?php
-              if (isset($errors)) {
-                foreach ($errors as $error) {
-                  echo $error;
-                }
-              } ?>
-            </div>
             <div class="form-group">
               <label for="txtName">Product Name</label>
-              <input type="text" class="form-control" name="txtName" id="txtName" placeholder="Enter category name">
+              <input type="text" class="form-control" name="txtName" id="txtName" placeholder="Enter category name" required>
             </div>
             <div class="form-group">
               <label>Category</label>
-              <select class="form-control" name="txtCategory">
+              <select class="form-control" name="txtCategory" required>
                 <option value="0" disabled selected>Select Category</option>
                 <?php
                 $query = $pdo->prepare("SELECT * FROM tbl_category");
@@ -74,17 +97,17 @@ if (isset($_POST['add_product'])) {
             </div>
             <div class="form-group">
               <label for="txtPurchasePrice">Purchase price</label>
-              <input type="number" class="form-control" name="txtPurchasePrice" id="txtPurchasePrice" placeholder="Enter category price">
+              <input type="number" class="form-control" name="txtPurchasePrice" id="txtPurchasePrice" placeholder="Enter category price" required>
             </div>
             <div class="form-group">
               <label for="txtPrice">Sell price</label>
-              <input type="number" class="form-control" name="txtPrice" id="txtPrice" placeholder="Enter category price">
+              <input type="number" class="form-control" name="txtPrice" id="txtPrice" placeholder="Enter category price" required>
             </div>
           </div>
           <div class="col-md-6">
             <div class="form-group">
               <label for="txtStock">Stock</label>
-              <input type="text" class="form-control" name="txtStock" id="txtStock" placeholder="Enter stock Qty">
+              <input type="text" class="form-control" name="txtStock" id="txtStock" placeholder="Enter stock Qty" required>
             </div>
             <div class="form-group">
               <label for="txtDesc">Description</label>
---
+++ product_list.php
@@ -0,0 +1,64 @@
+<?php include "db_connect.php"; ?>
+<?php session_start(); ?>
+<?php if ($_SESSION['user_email'] == "" || $_SESSION['user_role'] == 'user') {
+  header('location:index.php');
+} ?>
+<?php include_once "header-user.php"; ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Product List
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+      <li class="active">Here</li>
+    </ol>
+  </section>
+  <section class="content container-fluid">
+    <div class="box box-info">
+      <div class="box-header with-border">
+        <h3 class="box-title">Product List</h3>
+        <table class="table table-striped">
+          <thead>
+            <tr style="font-size: 1.6rem;">
+              <th>#</th>
+              <th>Product name</th>
+              <th>Product category</th>
+              <th>Purchase price</th>
+              <th>Sell price</th>
+              <th>In stock</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $query = $pdo->prepare("SELECT * FROM tbl_product");
+            $query->execute();
+            $products = $query->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($products as $product) { ?>
+              <tr style="font-size: 1.8rem;">
+                <td><?= $product['p_id'] ?></td>
+                <td><?= $product['p_name'] ?></td>
+                <td><?= $product['p_category'] ?></td>
+                <td><?= $product['purchase_price'] ?></td>
+                <td><?= $product['sell_price'] ?></td>
+                <td><?= $product['p_stock'] ?></td>
+                <td><?= $product['p_desc'] ?></td>
+                <td><img style="width: 100px;" src="<?= $product['p_image'] ?>" class="img-rounded"></td>
+                <td><a class="btn btn-danger" href="registration.php?id=<?= $product['p_id']; ?>">X</a></td>
+              </tr>
+            <?php }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </section>
+  <!-- /.content -->
+  <?php include_once "footer.php"; ?>
